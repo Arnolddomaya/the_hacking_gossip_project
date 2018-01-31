8346beebcf2ee15c7abd104b805c18a892009234
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-31 23:17:50 +0100

    gossips ok

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -8,7 +8,7 @@ class GossipsController < ApplicationController
   end
 
   def create
-    @gossip =  Gossip.new(gossip_param)
+    @gossip =  Gossip.new(gossip_params)
     @gossip.save
     #fait appel à la fonction show avec l'id du gossip
     redirect_to @gossip
@@ -22,9 +22,23 @@ class GossipsController < ApplicationController
     @gossip = Gossip.find(params[:id])
   end
 
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(gossip_params)
+    redirect_to @gossip
+  end
+
+  def destroy
+    Gossip.find(params[:id]).destroy
+    redirect_to gossips_path
+  end
+
+
+
+
 
   private
-    def gossip_param
+    def gossip_params
       params.require(:gossip).permit(:anonymous_author, :content)
     end
 end
